3f572431a612b3e2e98c1b9aaa71e3563da227b1
newbieDX <chenxubu@bu.edu>
2021-11-29 21:15:19 -0500

    update sth new

--- data clean and wrangle.R
+++ data clean and wrangle.R
@@ -1,3 +1,23 @@
+library(tidyr)
+library(tidyverse)
+library(dplyr)
+
 injuries <- read.csv("injuries_2010-2020.csv",header = T)
 playerstats <- read.csv("all_seasons.csv", header = T)
+detail_playerstats <- read.csv("detail_player_data.csv")
 
+nba_detail_playerstats <- subset(detail_playerstats, League == "NBA")
+rs_detail_playerstats <- subset(nba_detail_playerstats, Stage == "Regular_Season")
+po_detail_playerstats <- subset(nba_detail_playerstats, Stage == "Playoffs")
+
+
+
+lbj <- subset(playerstats, player_name == "LeBron James")
+kb <- subset(playerstats, player_name == "Kobe Bryant")
+
+relinquished <- subset(injuries, select = c(Date,Team, Relinquished, Notes))
+relinquished_lbj <- subset(relinquished, Relinquished == "LeBron James")
+relinquished_kb <- subset(relinquished, Relinquished == "Kobe Bryant")
+
+acquired <- subset(injuries, select = c(Date,Team, Acquired, Notes))
+    
